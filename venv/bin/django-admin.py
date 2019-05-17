b4ae56aa14051949027ae2c25eead4a7de272047
Luiz Gabriel Gusmão Santos <luizgabrielma9@gmail.com>
2019-05-16 23:05:47 -0300

    commit inicial

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/luizgabriel/Documentos/ma9/cursos/rest-API/pontos_turisticos/venv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
